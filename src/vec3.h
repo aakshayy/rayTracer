31f6646b60440d0bee1a62be320424cc9430f3be
KuKu <aarlika@ncsu.edu>
2017-03-05 17:07:35 -0500

    Made vec functions line

--- src/vec3.h
+++ src/vec3.h
@@ -9,30 +9,30 @@ public:
     vec3() { e[0] = 0; e[1] = 0; e[2] = 0; }
     vec3(float _x, float _y, float _z) { e[0] = _x; e[1] = _y; e[2] = _z; }
 
-    float x() const { return e[0]; }
-    float y() const { return e[1]; }
-    float z() const { return e[2]; }
+    inline float x() const { return e[0]; }
+    inline float y() const { return e[1]; }
+    inline float z() const { return e[2]; }
 
-    float operator[](int i) { return e[i]; }
-    void set(float x, float y, float z) { e[0] = x; e[1] = y; e[2] = z; }
+    inline float operator[](int i) { return e[i]; }
+    inline void set(float x, float y, float z) { e[0] = x; e[1] = y; e[2] = z; }
 
-    vec3 operator+(vec3 v) { return vec3(e[0] + v[0], e[1] + v[1], e[2] + v[2]); }
-    vec3 operator-(vec3 v) { return vec3(e[0] - v[0], e[1] - v[1], e[2] - v[2]); }
+    inline vec3 operator+(vec3 v) { return vec3(e[0] + v[0], e[1] + v[1], e[2] + v[2]); }
+    inline vec3 operator-(vec3 v) { return vec3(e[0] - v[0], e[1] - v[1], e[2] - v[2]); }
     //Scaling
-    vec3 operator*(float s) { return vec3(e[0] * s, e[1] * s, e[2] * s); }
+    inline vec3 operator*(float s) { return vec3(e[0] * s, e[1] * s, e[2] * s); }
     //Dot product
-    float operator*(vec3 &v) { return (e[0] * v[0]) + (e[1] * v[1]) + (e[2] * v[2]); }
-    vec3 operator/(float s) { return vec3(e[0] / s, e[1] / s, e[2] / s); }
+    inline float operator*(vec3 &v) { return e[0] * v[0] + e[1] * v[1] + e[2] * v[2]; }
+    inline vec3 operator/(float s) { return vec3(e[0] / s, e[1] / s, e[2] / s); }
     //Cross product
-    vec3 cross(vec3 &v) { 
-        return vec3((e[1] * v[2]) - (e[2] * v[1]),
-                    (e[2] * v[0]) - (e[0] * v[2]),
-                    (e[0] * v[1]) - (e[1] * v[0]));
+    inline vec3 cross(vec3 &v) { 
+        return vec3(e[1] * v[2] - e[2] * v[1],
+                    e[2] * v[0] - e[0] * v[2],
+                    e[0] * v[1] - e[1] * v[0]);
     }
     //Euclidean length
-    float length() { return (float)sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]); }
+    inline float length() { return (float)sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]); }
     //Unit vector
-    vec3 normalize() {
+    inline vec3 normalize() {
         float len = length();
         return (len != 0) ? vec3(e[0] / len, e[1] / len, e[2] / len) : vec3();
     }
